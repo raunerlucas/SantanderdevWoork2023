4bc86b205994bf4915ea37e8886658d14a97a270
raunerlucas <raunerlucas@gmail.com>
2023-10-18 10:34:57 -0300

    Create Project

---
+++ src/main/java/me/dio/santander/SantanderDevWoorApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWoorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderDevWoorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dio/santander/SantanderDevWoorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWoorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
